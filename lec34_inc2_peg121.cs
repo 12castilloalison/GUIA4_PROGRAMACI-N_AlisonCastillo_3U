a446422f404044da0f3c449ad64feeabdea6b395
12castilloalison <castilloalison16db@gmail.com>
2026-07-23 10:55:09 -0600

    Update lec34_inc2_peg121.cs

--- lec34_inc2_peg121.cs
+++ lec34_inc2_peg121.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace CalculoIMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== CÁLCULO DEL ÍNDICE DE MASA CORPORAL (IMC) ===");
+            Console.Write("Ingrese su peso en kilogramos (kg): ");
+            double peso = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Ingrese su altura en metros (m): ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+            double imc = peso / (altura * altura);
+            Console.WriteLine($"\nSu IMC es: {imc:F2}");
+            if (imc < 18.5)
+            {
+                Console.WriteLine("Clasificación: Bajo peso");
+            }
+            else if (imc >= 18.5 && imc < 25)
+            {
+                Console.WriteLine("Clasificación: Peso normal");
+            }
+            else if (imc >= 25 && imc < 30)
+            {
+                Console.WriteLine("Clasificación: Sobrepeso");
+            }
+            else
+            {
+                Console.WriteLine("Clasificación: Obesidad");
+            }
+        }
+    }
+}
